a7ad9fdba671103369cfc29ed8ac6676474be46f
Y-leeeee <lyr890802@gmail.com>
2023-11-12 21:32:43 +0100

    added code to get random data from api

--- src/chooseCharacter.js
+++ src/chooseCharacter.js
@@ -1,7 +1,29 @@
 function handleResponse(response) {
-  console.log(response.data);
-  const character = response.data[0];
-  const box = document.getElementById;
+  const character = response.data;
+  console.log(character);
+  var randCharacter = character[Math.floor(Math.random() * character.length)];
+  console.log(randCharacter);
+
+  const characterBox = document.getElementById("box");
+
+  const characterName = randCharacter.name;
+  const heading = document.createElement("h1");
+  heading.innerHTML = characterName;
+  characterBox.appendChild(heading);
+
+  const characterImg = document.createElement("img");
+  characterImg.src = randCharacter.image;
+  characterBox.appendChild(characterImg);
+
+  const ancestry = randCharacter.ancestry;
+  const characterAncestry = document.createElement("div");
+  characterAncestry.innerHTML = ancestry;
+  characterBox.appendChild(characterAncestry);
+
+  const dateOfBirth = randCharacter.dateOfBirth;
+  const characterBirthdate = document.createElement("div");
+  characterBirthdate.innerHTML = dateOfBirth;
+  characterBox.appendChild(characterBirthdate);
 }
 
 let url = "https://hp-api.onrender.com/api/characters";
